ff1a0b804c8c44fbcf5b515cb33f17fb2e76a456
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-11-03 02:11:02 -0700

    Fix assest precompilation.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get '/splash' => 'splash#index'
   root 'splash#index'
   # root 'setup#index'
-  get '/setup' => 'setup#index'
+  #get '/setup' => 'setup#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
